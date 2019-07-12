ef8cebcb57385284ef01102c2b23e352e647abad
段丹阳 <duandanyang@xiaozhu.com>
2019-07-12 17:49:24 +0800

    Tutorial 7: 概要和客户端库

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 from snippets import views
 
@@ -9,9 +10,12 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Pastebin API')
+
 # API URL现在路由器自动确定
 # 另外，我们还要包含可浏览的API的登录URL
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url('schema/', schema_view),
 ]
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 10,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
